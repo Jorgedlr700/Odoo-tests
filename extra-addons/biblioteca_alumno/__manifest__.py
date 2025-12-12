caf4454260b54c3999cadbf64be881c5f6700d3b
Jorgedlr700 <jorge.de3@isepceu.es>
2025-12-12 20:20:07 +0100

    Añadir módulo Biblioteca Alumno

---
+++ extra-addons/biblioteca_alumno/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Biblioteca Alumno",
+    "version": "1.0",
+    "summary": "Módulo de práctica: gestión de libros",
+    "author": "Alumno DAM",
+    "category": "Teaching",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/biblioteca_libro_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
